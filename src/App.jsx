97930f6d3167c663e6f109dfe095bcfbf4204078
Maziyar <maziroadtodivine@gmail.com>
2023-07-31 02:59:41 +0330

    Set react-query devTools and training on options.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import "./App.css";
 // router
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index";
+// react query
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/pages/superhero/RQSuperHeroes.jsx
+++ src/pages/superhero/RQSuperHeroes.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 const RQSuperHeroes = () => {
-  return <div></div>;
+  // sideEffects based on api call success or failed
+  const onSuccess = () => {};
+  const onError = () => {};
+
+  const { isLoading, data, isError, error, refetch } = useQuery(
+    "super-heroes",
+    () => {
+      return axios.get("http://localhost:4000/superheroes");
+    },
+    {
+      // // set cache timer
+      // cacheTime: 5000,
+      // staleTime: 30000,
+      // refetchOnMount : true
+      // refetchOnWindowFocus: true,
+      // refetchInterval : 2000,
+      // // api call with btn
+      // enabled: false,
+      // // set api callback for times of success or failed
+      // onSuccess,
+      // onError,
+      // // filter for just name of data
+      // select: (data) => {
+      //   const superHeroName = data.data.map((hero) => hero.name);
+      //   return superHeroName;
+      // },
+    }
+  );
+
+  if (isLoading) {
+    return <h2>Loading</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <div>
+        {/* //  when you wanna set api call based on click on btn  \\ */}
+        {/* <button onClick={refetch}>click me</button> */}
+
+        {/* // when you wanna set filter based on data specific key \\ */}
+        {/* {data.map((item, index) => {
+          return <h2 key={index}>{item}</h2>;
+        })} */}
+
+        {data?.data.map((item, index) => {
+          return <h2 key={index}>{item.name}</h2>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default RQSuperHeroes;
